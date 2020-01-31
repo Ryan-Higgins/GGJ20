bcd68bf40b6f8628fc0bb5afa05da899c0a44da3
Keith Connors <c17507803@mydit.ie>
2020-01-31 20:29:19 +0000

    UI, Skeleton code

--- GGJ20/Assets/Scripts/Managers/GameManager.cs
+++ GGJ20/Assets/Scripts/Managers/GameManager.cs
@@ -6,12 +6,23 @@ public class GameManager : MonoBehaviour
 {
     [Header("Player data")] 
     public bool PlayerIsAlive;
+
+    public float PlayerHealth = 100f;
     
     [Header("NPC data")] 
     public bool npcBool;
 
+    public enum TYPE
+    {
+        FLYING, GROUND, DISTANCE
+    }
+
     [Header("Game data")] 
     public int CurrentDay = 1;
+
+    public int Currency;
+
+    public int AmountOfFortifications;
     
     [Header("Spawn Locations")]
     public Transform Keep_Spawn; //Only one Keep 'Camp' area.
