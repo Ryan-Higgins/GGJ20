5e299927392ae02dc7ebc134769dfe2a6a68e8c4
Ryan-Higgins <c16381886@mydit.ie>
2020-02-01 13:00:07 +0000

    Walls added

---
+++ GGJ20/Assets/Scripts/Attack.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Attack : MonoBehaviour
+{
+    private RaycastHit2D hit;
+    private Rigidbody2D myRB;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        myRB = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.Space))
+        {
+            hit = Physics2D.Raycast(transform.position, Vector2.right);
+            Debug.DrawRay(transform.position, Vector2.right, Color.red);
+        } 
+    }
+}
