c483adcc97e261ca23facedf4ba7c037a534329c
Keith Connors <c17507803@mydit.ie>
2020-02-01 11:45:58 +0000

    .

--- GGJ20/Assets/Scripts/Managers/GameManager.cs
+++ GGJ20/Assets/Scripts/Managers/GameManager.cs
@@ -11,23 +11,23 @@ public class GameManager : MonoBehaviour
     
     [Header("NPC data")] 
     public bool npcBool;
-
-    public enum TYPE
-    {
-        FLYING, GROUND, DISTANCE
-    }
-
+    
     [Header("Game data")] 
     public int CurrentDay = 1;
 
     public int Currency;
 
     public int AmountOfFortifications;
+
+    public Sprite Tier1KeepOUTLINE, Tier2KeepOUTLINE, Tier3KeepOUTLINE,
+        Tier1WallOUTLINE, Tier2WallOUTLINE, Tier3WallOUTLINE,
+        Tier1TowerOUTLINE, Tier2TowerOUTLINE, Tier3TowerOUTLINE,
+        Tier1SpikeOUTLINE, Tier2SpikeOUTLINE, Tier3SpikeOUTLINE;
     
     [Header("Spawn Locations")]
-    public Transform Keep_Spawn; //Only one Keep 'Camp' area.
-    public Transform[] Wall_Spawn; //All spawn locations for Walls
-    public Transform[] Tower_Spawn; // ^
-    public Transform[] Spike_Spawn; // ^ Same
+    public Transform Keep_SpawnLOCATION; //Only one Keep 'Camp' area.
+    public Transform[] Wall_SpawnLOCATION; //All spawn locations for Walls
+    public Transform[] Tower_SpawnLOCATION; // ^
+    public Transform[] Spike_SpawnLOCATION; // ^ Same
     
 }
