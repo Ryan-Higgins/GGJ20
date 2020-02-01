5d6bd60a34de81c5d47703d75bea1b819d18d357
Keith Connors <c17507803@mydit.ie>
2020-02-01 16:08:41 +0000

    .

---
+++ GGJ20/Assets/Scripts/KeepBuildTrigger.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KeepBuildTrigger : MonoBehaviour
+{
+    public GameObject tier1Keep, tier2Keep, tier3Keep;
+    public GameManager _gameManager;
+    private void Start()
+    {
+        _gameManager = GameObject.Find("GameManager").GetComponent<GameManager>();
+        tier1Keep.SetActive(false);
+        tier2Keep.SetActive(false);
+        tier3Keep.SetActive(false);
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag == "Player")
+        {
+            Debug.Log("Player entered collider.");
+            if (_gameManager.CurrentKeepTier == 0)
+            {
+                tier1Keep.SetActive(true);
+            } 
+            else if (_gameManager.CurrentKeepTier == 1)
+            {
+                tier2Keep.SetActive(true);
+            }
+            else if (_gameManager.CurrentKeepTier == 2)
+            {
+                tier3Keep.SetActive(true);
+            } else if (_gameManager.CurrentKeepTier == 3)
+            {
+               // tier3Keep.SetActive(true);
+               Debug.Log("MAX TIER REACHED");
+            }
+            else
+            {
+                Debug.Log("No action");
+            }
+
+            /* if (_gameManager.currentKeepTier == 1)
+             {
+                 tier1Keep.SetActive(true);
+             }
+             if (_gameManager.currentKeepTier == 2)
+             {
+                 tier2Keep.SetActive(true);
+             }
+             if (_gameManager.currentKeepTier == 3)
+             {
+                 tier3Keep.SetActive(true);
+             }*/
+        }
+        
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.tag == "Player")
+        {
+            tier1Keep.SetActive(false);
+            tier2Keep.SetActive(false);
+            tier3Keep.SetActive(false);
+        }
+    }
+}
---
+++ GGJ20/Assets/Scripts/TowerBuildTrigger.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TowerBuildTrigger : MonoBehaviour
+{
+    public GameObject tier1Tower, tier2Tower, tier3Tower;
+    public GameManager _gameManager;
+    private void Start()
+    {
+        _gameManager = GameObject.Find("GameManager").GetComponent<GameManager>();
+        tier1Tower.SetActive(false);
+        tier2Tower.SetActive(false);
+        tier3Tower.SetActive(false);
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag == "Player")
+        {
+            Debug.Log("Player entered collider.");
+            if (_gameManager.CurrentTowerTier == 0)
+            {
+                tier1Tower.SetActive(true);
+            } 
+            else if (_gameManager.CurrentTowerTier == 1)
+            {
+                tier2Tower.SetActive(true);
+            }
+            else if (_gameManager.CurrentTowerTier == 2)
+            {
+                tier3Tower.SetActive(true);
+            } else if (_gameManager.CurrentTowerTier == 3)
+            {
+               // tier3Tower.SetActive(true);
+               Debug.Log("MAX TIER REACHED");
+            }
+            else
+            {
+                Debug.Log("No action");
+            }
+
+            /* if (_gameManager.currentTowerTier == 1)
+             {
+                 tier1Tower.SetActive(true);
+             }
+             if (_gameManager.currentTowerTier == 2)
+             {
+                 tier2Tower.SetActive(true);
+             }
+             if (_gameManager.currentTowerTier == 3)
+             {
+                 tier3Tower.SetActive(true);
+             }*/
+        }
+        
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.tag == "Player")
+        {
+            tier1Tower.SetActive(false);
+            tier2Tower.SetActive(false);
+            tier3Tower.SetActive(false);
+        }
+    }
+}
