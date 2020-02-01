e99973fa613a93c54767637777e95e00c7e2a0e3
Keith Connors <c17507803@mydit.ie>
2020-02-01 13:05:22 +0000

    .

---
+++ GGJ20/Assets/Scripts/BuildingTrigger.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BuildingTrigger : MonoBehaviour
+{
+    private GameManager _gameManager;
+    public GameObject BuildingOutline;
+
+    public bool isKeep, isWall, isSpike, isTower;
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag == "Player")
+        {
+            if (isKeep && other.tag == "Player")
+            {
+                Instantiate(_gameManager.Tier1KeepOUTLINE, _gameManager.Keep_SpawnLOCATION);
+            }
+
+            if (isWall)
+            {
+                Instantiate(_gameManager.Tier1WallOUTLINE);
+            }
+
+            if (isSpike)
+            {
+                Instantiate(_gameManager.Tier1SpikeOUTLINE);
+            }
+
+            if (isTower)
+            {
+                Instantiate(_gameManager.Tier1TowerOUTLINE);
+            }
+        }
+    }
+    
+
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            
+        }
+    }
+}
