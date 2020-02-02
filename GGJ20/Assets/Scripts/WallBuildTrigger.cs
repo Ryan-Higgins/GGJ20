7e8efd7db5def1c32d0b700bbeb85f52d7833a2a
Keith Connors <c17507803@mydit.ie>
2020-02-02 15:21:03 +0000

    .

--- GGJ20/Assets/Scripts/WallBuildTrigger.cs
+++ GGJ20/Assets/Scripts/WallBuildTrigger.cs
@@ -7,6 +7,10 @@ public class WallBuildTrigger : MonoBehaviour
 {
     public GameObject tier1Wall, tier2Wall, tier3Wall;
     public GameManager _gameManager;
+
+    [SerializeField] private int Cost;
+    public bool tier1Purchased;
+
     private void Start()
     {
         _gameManager = GameObject.Find("GameManager").GetComponent<GameManager>();
@@ -15,42 +19,68 @@ public class WallBuildTrigger : MonoBehaviour
         tier3Wall.SetActive(false);
     }
 
-    private void OnTriggerEnter2D(Collider2D other)
-    {
-        if (other.tag == "Player")
-        {
-            Debug.Log("Player entered collider.");
-            if (_gameManager.CurrentWallTier == 0)
-            {
-                tier1Wall.SetActive(true);
-            } 
-            else if (_gameManager.CurrentWallTier == 1)
-            {
-                tier2Wall.SetActive(true);
-            }
-            else if (_gameManager.CurrentWallTier == 2)
-            {
-                tier3Wall.SetActive(true);
-            } else if (_gameManager.CurrentWallTier == 3)
-            {
-               // tier3Wall.SetActive(true);
-               Debug.Log("MAX TIER REACHED");
-            }
-            else
-            {
-                Debug.Log("No action");
-            }
-        }
-        
-    }
 
-    private void OnTriggerExit2D(Collider2D other)
+    private void OnTriggerStay2D(Collider2D other)
     {
         if (other.tag == "Player")
         {
-            tier1Wall.SetActive(false);
-            tier2Wall.SetActive(false);
-            tier3Wall.SetActive(false);
+            // Debug.Log("Entered Wall Trigger");
+            if (Input.GetKeyUp(KeyCode.E) && _gameManager.CurrentWallTier == 0)
+            {
+                //  Debug.Log("Key pressed within trigger.");
+                if (_gameManager.Currency <= Cost)
+                {
+                    Debug.Log("No Money.");
+                }
+
+                if (_gameManager.Currency >= Cost)
+                {
+                    Debug.Log(("Purchased Wall!"));
+                    _gameManager.Currency = _gameManager.Currency - Cost;
+                    tier1Wall.SetActive(true);
+                    tier2Wall.SetActive(false);
+                    tier3Wall.SetActive(false);
+                    _gameManager.CurrentWallTier += 1;
+                }
+            }
+
+            if (Input.GetKeyUp(KeyCode.E) && _gameManager.CurrentWallTier == 1)
+            {
+                Debug.Log("Key pressed within trigger.");
+                if (_gameManager.Currency <= Cost)
+                {
+                    Debug.Log("No Money.");
+                }
+
+                if (_gameManager.Currency >= Cost)
+                {
+                    Debug.Log(("Purchased Wall!"));
+                    _gameManager.Currency = _gameManager.Currency - Cost;
+                    tier2Wall.SetActive(true);
+                    tier1Wall.SetActive(false);
+                    tier3Wall.SetActive(false);
+                    _gameManager.CurrentWallTier += 1;
+                }
+            }
+
+            if (Input.GetKeyUp(KeyCode.E) && _gameManager.CurrentWallTier == 2)
+            {
+                Debug.Log("Key pressed within trigger.");
+                if (_gameManager.Currency <= Cost)
+                {
+                    Debug.Log("No Money.");
+                }
+
+                if (_gameManager.Currency >= Cost)
+                {
+                    Debug.Log(("Purchased Wall!"));
+                    _gameManager.Currency = _gameManager.Currency - Cost;
+                    tier3Wall.SetActive(true);
+                    tier2Wall.SetActive(false);
+                    tier1Wall.SetActive(false);
+                    _gameManager.CurrentWallTier += 1;
+                }
+            }
         }
     }
 }
